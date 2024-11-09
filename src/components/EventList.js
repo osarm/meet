0563cbc895a89338fb485f6aaf9adbfefe7be670
osarm <161492930+osarm@users.noreply.github.com>
2024-11-09 12:22:17 -0500

    Adding Customization
    If applied, this commit will add customization to my app in the App.css file

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,12 +1,13 @@
 import Event from "./Event";
 
 const EventList = ({ events }) => {
-    return (
-      <ul id="event-list">
-        {events? 
-            events.map(event => <Event key={event.id} event={event} />): null}
-      </ul>
-    );
-  }
-  
+  return (
+    <ul id="event-list">
+      {events? events.map((event) => (
+        <li key={event.id} className="event-list-item">
+              <Event event={event} /></li>)): null}
+    </ul>
+  );
+};
+
 export default EventList;
